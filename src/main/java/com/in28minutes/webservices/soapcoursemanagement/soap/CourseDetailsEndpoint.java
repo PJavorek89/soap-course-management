912d63702c48cc38fd8dbdc1b7d9ef42cb0da230
Pavel Javorek <javorek2@komix.cz>
2022-06-27 16:35:40 +0200

    add Static {} method to get List of courses

--- src/main/java/com/in28minutes/webservices/soapcoursemanagement/soap/CourseDetailsEndpoint.java
+++ src/main/java/com/in28minutes/webservices/soapcoursemanagement/soap/CourseDetailsEndpoint.java
@@ -3,6 +3,9 @@ package com.in28minutes.webservices.soapcoursemanagement.soap;
 import com.example.myschema.CourseDetails;
 import com.example.myschema.GetCourseDetailRequest;
 import com.example.myschema.GetCourseDetailResponse;
+import com.in28minutes.webservices.soapcoursemanagement.soap.bean.Course;
+import com.in28minutes.webservices.soapcoursemanagement.soap.service.CourseDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
@@ -18,15 +21,19 @@ public class CourseDetailsEndpoint {
     //http://in28minutes.com/courses
     //GetCourseDetailResponse
 
+    @Autowired
+    CourseDetailsService courseDetailsService;
+
     @PayloadRoot(namespace="http://in28minutes.com/courses",
     localPart="GetCourseDetailRequest")
     @ResponsePayload
     public GetCourseDetailResponse processCourseDetailRequest(@RequestPayload GetCourseDetailRequest request){
         GetCourseDetailResponse response = new GetCourseDetailResponse();
+        Course course = courseDetailsService.findById(request.getId());
         CourseDetails courseDetails = new CourseDetails();
-        courseDetails.setId(request.getId());
-        courseDetails.setName("Microservices course");
-        courseDetails.setDescription("Good course");
+        courseDetails.setId(course.getId());
+        courseDetails.setName(course.getName());
+        courseDetails.setDescription(course.getDescription());
         response.setCourseDetails(courseDetails);
         return response;
     }
